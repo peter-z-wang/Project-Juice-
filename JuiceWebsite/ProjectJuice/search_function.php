d9bbccd016cd5e85f852f0d8f19743deee665911
peter-z-wang <peter.wang@colorado.edu>
2016-11-16 23:46:35 -0700

    Add files via upload

--- JuiceWebsite/ProjectJuice/search_function.php
+++ JuiceWebsite/ProjectJuice/search_function.php
@@ -5,9 +5,9 @@
 <body>
 <form action="http://localhost:8000/Php/display_recipes.php" method="post">
 
-
+	
 <p>
-Ingredients
+Enter Your Ingredients
 </p>
 
 <p>
@@ -16,6 +16,31 @@ Ingredient 1:
   <option value="">Select...</option>
   <option value="ice">Ice</option>
   <option value="vodka">Vodka</option>
+  <option value="gin">Gin</option>
+  <option value="dry vermout">Dry Vermout</option>
+  <option value="green olive">Green Olive</option>
+  <option value="triple sec">Triple Sec</option>
+  <option value="peach schnapps">Peach Schnapps</option>
+  <option value="cranberry juice">Cranberry Juice</option>
+  <option value="lemon juice">Lemon Juice</option>
+  <option value="lime juice">Lime Juice</option>
+  <option value="strawberry daiquiri mixer">Strawberry Daiquiri Mixer</option>
+  <option value="apple">Apple</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="mango">Mango</option>
+  <option value="white rum">White Rum</option>
+  <option value="jigger tequila">Jigger Tequila</option>
+  <option value="orange juice">Orange Juice</option>
+  <option value="jigger grenadine syrup">Jigger Grenadine Syrup</option>
+  <option value="maraschino cherry">Maraschino Cherry</option>
+  <option value="water">Water</option>
+  <option value="white sugar">White Sugar</option>
+  <option value="orange zest">Orange Zest</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="white tequila">White Tequila</option>
+  <option value="salt">Salt</option>
+  <option value="lime">Lime</option>
+  
 </select>
 </p>
 
@@ -24,10 +49,140 @@ Ingredient 1:
 Ingredient 2:
 <select name="Ingredient_2">
   <option value="">Select...</option>
-  <option value="gin">Gin</option>
+  <option value="ice">Ice</option>
   <option value="vodka">Vodka</option>
+  <option value="gin">Gin</option>
+  <option value="dry vermout">Dry Vermout</option>
+  <option value="green olive">Green Olive</option>
+  <option value="triple sec">Triple Sec</option>
+  <option value="peach schnapps">Peach Schnapps</option>
+  <option value="cranberry juice">Cranberry Juice</option>
+  <option value="lemon juice">Lemon Juice</option>
+  <option value="lime juice">Lime Juice</option>
+  <option value="strawberry daiquiri mixer">Strawberry Daiquiri Mixer</option>
+  <option value="apple">Apple</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="mango">Mango</option>
+  <option value="white rum">White Rum</option>
+  <option value="jigger tequila">Jigger Tequila</option>
+  <option value="orange juice">Orange Juice</option>
+  <option value="jigger grenadine syrup">Jigger Grenadine Syrup</option>
+  <option value="maraschino cherry">Maraschino Cherry</option>
+  <option value="water">Water</option>
+  <option value="white sugar">White Sugar</option>
+  <option value="orange zest">Orange Zest</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="white tequila">White Tequila</option>
+  <option value="salt">Salt</option>
+  <option value="lime">Lime</option>
+  
 </select>
 </p>
+
+
+<p>
+Ingredient 3:
+<select name="Ingredient_3">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="vodka">Vodka</option>
+  <option value="gin">Gin</option>
+  <option value="dry vermout">Dry Vermout</option>
+  <option value="green olive">Green Olive</option>
+  <option value="triple sec">Triple Sec</option>
+  <option value="peach schnapps">Peach Schnapps</option>
+  <option value="cranberry juice">Cranberry Juice</option>
+  <option value="lemon juice">Lemon Juice</option>
+  <option value="lime juice">Lime Juice</option>
+  <option value="strawberry daiquiri mixer">Strawberry Daiquiri Mixer</option>
+  <option value="apple">Apple</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="mango">Mango</option>
+  <option value="white rum">White Rum</option>
+  <option value="jigger tequila">Jigger Tequila</option>
+  <option value="orange juice">Orange Juice</option>
+  <option value="jigger grenadine syrup">Jigger Grenadine Syrup</option>
+  <option value="maraschino cherry">Maraschino Cherry</option>
+  <option value="water">Water</option>
+  <option value="white sugar">White Sugar</option>
+  <option value="orange zest">Orange Zest</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="white tequila">White Tequila</option>
+  <option value="salt">Salt</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
+<p>
+Ingredient 4:
+<select name="Ingredient_4">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="vodka">Vodka</option>
+  <option value="gin">Gin</option>
+  <option value="dry vermout">Dry Vermout</option>
+  <option value="green olive">Green Olive</option>
+  <option value="triple sec">Triple Sec</option>
+  <option value="peach schnapps">Peach Schnapps</option>
+  <option value="cranberry juice">Cranberry Juice</option>
+  <option value="lemon juice">Lemon Juice</option>
+  <option value="lime juice">Lime Juice</option>
+  <option value="strawberry daiquiri mixer">Strawberry Daiquiri Mixer</option>
+  <option value="apple">Apple</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="mango">Mango</option>
+  <option value="white rum">White Rum</option>
+  <option value="jigger tequila">Jigger Tequila</option>
+  <option value="orange juice">Orange Juice</option>
+  <option value="jigger grenadine syrup">Jigger Grenadine Syrup</option>
+  <option value="maraschino cherry">Maraschino Cherry</option>
+  <option value="water">Water</option>
+  <option value="white sugar">White Sugar</option>
+  <option value="orange zest">Orange Zest</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="white tequila">White Tequila</option>
+  <option value="salt">Salt</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
+
+<p>
+Ingredient 5:
+<select name="Ingredient_5">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="vodka">Vodka</option>
+  <option value="gin">Gin</option>
+  <option value="dry vermout">Dry Vermout</option>
+  <option value="green olive">Green Olive</option>
+  <option value="triple sec">Triple Sec</option>
+  <option value="peach schnapps">Peach Schnapps</option>
+  <option value="cranberry juice">Cranberry Juice</option>
+  <option value="lemon juice">Lemon Juice</option>
+  <option value="lime juice">Lime Juice</option>
+  <option value="strawberry daiquiri mixer">Strawberry Daiquiri Mixer</option>
+  <option value="apple">Apple</option>
+  <option value="pineapple">Pineapple</option>
+  <option value="mango">Mango</option>
+  <option value="white rum">White Rum</option>
+  <option value="jigger tequila">Jigger Tequila</option>
+  <option value="orange juice">Orange Juice</option>
+  <option value="jigger grenadine syrup">Jigger Grenadine Syrup</option>
+  <option value="maraschino cherry">Maraschino Cherry</option>
+  <option value="water">Water</option>
+  <option value="white sugar">White Sugar</option>
+  <option value="orange zest">Orange Zest</option>
+  <option value="watermelon">Watermelon</option>
+  <option value="white tequila">White Tequila</option>
+  <option value="salt">Salt</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
 
 <p>
 
