3653e364b57ca2b209675f1d57962db10272eed9
peter-z-wang <peter.wang@colorado.edu>
2016-11-29 19:20:57 -0700

    Add files via upload

---
+++ JuiceWebsite/ProjectJuice/search_function_classic.php
@@ -0,0 +1,183 @@
+<html>
+<head>
+<title>Classic Ingredient Search</title>
+</head>
+<body>
+<form action="http://localhost:8000/ProjectJuice/display_recipes_classic.php" method="post">
+
+	
+<p>
+Project Juice Classic
+</p>	
+
+<p>
+Enter Your Ingredients
+</p>
+
+<p>
+Ingredient 1:
+<select name="Ingredient_1">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="kiwifruit">Kiwi Fruit</option>
+  <option value="frozen lemonade">Frozen Lemonade</option>
+  <option value="grapefruit juice">Grapefruit Juice</option>
+  <option value="sugar">Sugar</option>
+  <option value="strawberry drink mix">Strawberry Drink Mix</option>
+  <option value="ginger ale">Ginger Ale</option>
+  <option value="ground espresso">Ground Espresso</option>
+  <option value="water">Water</option>
+  <option value="vanilla soymilk">Vanilla Soymilk</option>
+  <option value="caramel">Caramel</option>
+  <option value="chocolate">Chocolate</option>
+  <option value="ice cubes">Ice Cubes</option>
+  <option value="iced tea">Iced Tea</option>
+  <option value="frozen pineapple-orange">Frozen Pineapple-Orange</option>
+  <option value="mint leaf">Mint Leaf</option>
+  <option value="passion fruit juice">Passion Fruit Juice</option>
+  <option value="sparkling water">Sparkling Water</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="orange">Orange</option>
+  <option value="star fruit">Star Fruit</option>
+  <option value="cranberries">Cranberries</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
+
+<p>
+Ingredient 2:
+<select name="Ingredient_2">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="kiwifruit">Kiwi Fruit</option>
+  <option value="frozen lemonade">Frozen Lemonade</option>
+  <option value="grapefruit juice">Grapefruit Juice</option>
+  <option value="sugar">Sugar</option>
+  <option value="strawberry drink mix">Strawberry Drink Mix</option>
+  <option value="ginger ale">Ginger Ale</option>
+  <option value="ground espresso">Ground Espresso</option>
+  <option value="water">Water</option>
+  <option value="vanilla soymilk">Vanilla Soymilk</option>
+  <option value="caramel">Caramel</option>
+  <option value="chocolate">Chocolate</option>
+  <option value="ice cubes">Ice Cubes</option>
+  <option value="iced tea">Iced Tea</option>
+  <option value="frozen pineapple-orange">Frozen Pineapple-Orange</option>
+  <option value="mint leaf">Mint Leaf</option>
+  <option value="passion fruit juice">Passion Fruit Juice</option>
+  <option value="sparkling water">Sparkling Water</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="orange">Orange</option>
+  <option value="star fruit">Star Fruit</option>
+  <option value="cranberries">Cranberries</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
+
+<p>
+Ingredient 3:
+<select name="Ingredient_3">
+ <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="kiwifruit">Kiwi Fruit</option>
+  <option value="frozen lemonade">Frozen Lemonade</option>
+  <option value="grapefruit juice">Grapefruit Juice</option>
+  <option value="sugar">Sugar</option>
+  <option value="strawberry drink mix">Strawberry Drink Mix</option>
+  <option value="ginger ale">Ginger Ale</option>
+  <option value="ground espresso">Ground Espresso</option>
+  <option value="water">Water</option>
+  <option value="vanilla soymilk">Vanilla Soymilk</option>
+  <option value="caramel">Caramel</option>
+  <option value="chocolate">Chocolate</option>
+  <option value="ice cubes">Ice Cubes</option>
+  <option value="iced tea">Iced Tea</option>
+  <option value="frozen pineapple-orange">Frozen Pineapple-Orange</option>
+  <option value="mint leaf">Mint Leaf</option>
+  <option value="passion fruit juice">Passion Fruit Juice</option>
+  <option value="sparkling water">Sparkling Water</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="orange">Orange</option>
+  <option value="star fruit">Star Fruit</option>
+  <option value="cranberries">Cranberries</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
+<p>
+Ingredient 4:
+<select name="Ingredient_4">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="kiwifruit">Kiwi Fruit</option>
+  <option value="frozen lemonade">Frozen Lemonade</option>
+  <option value="grapefruit juice">Grapefruit Juice</option>
+  <option value="sugar">Sugar</option>
+  <option value="strawberry drink mix">Strawberry Drink Mix</option>
+  <option value="ginger ale">Ginger Ale</option>
+  <option value="ground espresso">Ground Espresso</option>
+  <option value="water">Water</option>
+  <option value="vanilla soymilk">Vanilla Soymilk</option>
+  <option value="caramel">Caramel</option>
+  <option value="chocolate">Chocolate</option>
+  <option value="ice cubes">Ice Cubes</option>
+  <option value="iced tea">Iced Tea</option>
+  <option value="frozen pineapple-orange">Frozen Pineapple-Orange</option>
+  <option value="mint leaf">Mint Leaf</option>
+  <option value="passion fruit juice">Passion Fruit Juice</option>
+  <option value="sparkling water">Sparkling Water</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="orange">Orange</option>
+  <option value="star fruit">Star Fruit</option>
+  <option value="cranberries">Cranberries</option>
+  <option value="lime">Lime</option>
+  
+</select>
+</p>
+
+
+<p>
+Ingredient 5:
+<select name="Ingredient_5">
+  <option value="">Select...</option>
+  <option value="ice">Ice</option>
+  <option value="kiwifruit">Kiwi Fruit</option>
+  <option value="frozen lemonade">Frozen Lemonade</option>
+  <option value="grapefruit juice">Grapefruit Juice</option>
+  <option value="sugar">Sugar</option>
+  <option value="strawberry drink mix">Strawberry Drink Mix</option>
+  <option value="ginger ale">Ginger Ale</option>
+  <option value="ground espresso">Ground Espresso</option>
+  <option value="water">Water</option>
+  <option value="vanilla soymilk">Vanilla Soymilk</option>
+  <option value="caramel">Caramel</option>
+  <option value="chocolate">Chocolate</option>
+  <option value="ice cubes">Ice Cubes</option>
+  <option value="iced tea">Iced Tea</option>
+  <option value="frozen pineapple-orange">Frozen Pineapple-Orange</option>
+  <option value="mint leaf">Mint Leaf</option>
+  <option value="passion fruit juice">Passion Fruit Juice</option>
+  <option value="sparkling water">Sparkling Water</option>
+  <option value="kiwi">Kiwi</option>
+  <option value="orange">Orange</option>
+  <option value="star fruit">Star Fruit</option>
+  <option value="cranberries">Cranberries</option>
+  <option value="lime">Lime</option>
+</select>
+</p>
+
+
+<p>
+
+<input type="submit" name="submit" value="Send" />
+
+</p>
+
+</form>
+</body>
+</html>
